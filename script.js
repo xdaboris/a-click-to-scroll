bcd0bfe852fbf2b3a688457bca5e95d4d1cc4553
Boris <boristosicmusic@gmail.com>
2024-05-15 22:26:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
